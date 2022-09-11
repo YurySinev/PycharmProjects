795f5b25926213ed50e6db21c488dcdc5914b03a
Yury Sinev <yuraseg@gmail.com>
2022-09-11 14:55:47 +0300

    Мои учебно-тренировочные файлы по курсу Интенсив Python

--- INTPY/C5_Requests_JSON_Redis_etc/CRYPTO_BOT/app.py
+++ INTPY/C5_Requests_JSON_Redis_etc/CRYPTO_BOT/app.py
@@ -11,7 +11,8 @@ keys = {
     'биткоин': 'BTC',
     'эфириум': 'ETH',
     'солана': 'SOL',
-    'доллар': 'USDT'
+    'доллар': 'USDT',
+    'рубль': 'RUR'
 }
 
 
@@ -19,11 +20,43 @@ class ConvertionException(Exception):
     pass
 
 
+class CryptoConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        if quote == base:  # если пользователь ввел одну и ту же валюту
+            raise ConvertionException(
+                f"Невозможно перевести одинаковые валюты {base}")
+
+        try:  # заведем тикеры для обозначения валют и проверим правильность ввода
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f"Не удалось обработать валюту {quote}")
+
+        try:  # то же самое для второй валюты
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f"Не удалось обработать валюту {base}")
+
+        try:  # обработка неверного количества
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(
+                f"Не удалось обработать количество {amount}")
+
+        r = requests.get(
+            f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+
+        return total_base
+
+
 # @bot.message_handler()
 # def echo_test(message: telebot.types.Message):
 #    bot.send_message(message.chat.id, 'Привет!')
 
 # обработка команд start и help:
+
+
 @bot.message_handler(commands=['start', 'help'])
 def help(message: telebot.types.Message):
     text = 'Чтобы начать работу, введите команду боту в следующем формате:\n \
@@ -33,6 +66,8 @@ def help(message: telebot.types.Message):
 
 
 # выведение списка доступных валют:
+
+
 @bot.message_handler(commands=['values'])
 def values(message: telebot.types.Message):
     text = 'Доступные валюты:'
@@ -42,38 +77,18 @@ def values(message: telebot.types.Message):
 
 
 # инфо о курсе валютной пары:
+
+
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
     # поместили текст входящего сообщения в отдельную переменную
     values = message.text.split(' ')
 
-    if len(values) > 3:  # если пользователь ввел более трех параметров
+    if len(values) != 3:  # если пользователь ввел более трех параметров
         raise ConvertionException("Слишком много параметров")
 
     quote, base, amount = values
-
-    if quote == base:  # если пользователь ввел одну и ту же валюту
-        raise ConvertionException(
-            f"Невозможно перевести одинаковые валюты {base}")
-
-    try:  # заведем тикеры для обозначения валют и проверим правильность ввода
-        quote_ticker = keys[quote]
-    except KeyError:
-        raise ConvertionException(f"Не удалось обработать валюту {quote}")
-
-    try:  # то же самое для второй валюты
-        base_ticker = keys[base]
-    except KeyError:
-        raise ConvertionException(f"Не удалось обработать валюту {base}")
-
-    try:  # обработка неверного количества
-        amount = float(amount)
-    except ValueError:
-        raise ConvertionException(f"Не удалось обработать количество {amount}")
-
-    r = requests.get(
-        f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-    total_base = json.loads(r.content)[keys[base]]
+    total_base = CryptoConverter.convert(quote, base, amount)
     text = f'Цена {amount} {quote} в {base} - {total_base}'
     bot.send_message(message.chat.id, text)
 
