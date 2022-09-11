078bfeb8e6ec2a484353463efc9a3f95d23a1d0d
Yury Sinev <yuraseg@gmail.com>
2022-09-11 17:40:12 +0300

    Мои учебно-тренировочные файлы по курсу Интенсив Python

--- INTPY/C5_Requests_JSON_Redis_etc/CRYPTO_BOT/app.py
+++ INTPY/C5_Requests_JSON_Redis_etc/CRYPTO_BOT/app.py
@@ -28,18 +28,25 @@ def values(message: telebot.types.Message):
 
 
 # инфо о курсе валютной пары:
+
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
-    # поместили текст входящего сообщения в отдельную переменную
-    values = message.text.split(' ')
+    try:
+        values = message.text.split(' ')  # входящее сообщение - в отдельную переменную
 
-    if len(values) != 3:  # если пользователь ввел более трех параметров
-        raise ConvertionException("Слишком много параметров")
+        if len(values) != 3:  # если пользователь ввел более трех параметров
+            raise ConvertionException("Число параметров должно быть 3:\nвалюта валюта сумма")
 
-    quote, base, amount = values
-    total_base = CryptoConverter.convert(quote, base, amount)
-    text = f'Цена {amount} {quote} в {base} - {total_base}'
-    bot.send_message(message.chat.id, text)
+        quote, base, amount = values
+        total_base = CryptoConverter.convert(quote, base, amount)
+
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка пользователя:\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Ошибка:\n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
 
 
 bot.polling()
